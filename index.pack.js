4a017e852e1abd85b198d1c20998264e47f93c51
ben <noahbenaas@gmail.com>
2021-12-02 17:30:23 +0100

    building my first function to create the grid

---
+++ index.pack.js
@@ -0,0 +1,16 @@
+const grid = document.querySelector(".grid");
+const startButton = document.getElementById("start");
+const score = document.getElementById("score");
+
+function createGrid() {
+  // create 100 of these elemenrs
+  for (let i = 0; i < 100; i++) {
+    const square = document.createElement("div");
+     //create element
+
+     
+    console.log(square);
+
+  }
+}
+createGrid();
