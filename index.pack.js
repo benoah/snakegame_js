2ef72f45984fa5ffa76d67b112585a773629bcb7
ben <noahbenaas@gmail.com>
2021-12-02 20:44:29 +0100

    css

--- index.pack.js
+++ index.pack.js
@@ -1,16 +1,27 @@
 const grid = document.querySelector(".grid");
 const startButton = document.getElementById("start");
 const score = document.getElementById("score");
+let squares = [];
+let currentSnake = [2, 1, 0];
 
 function createGrid() {
-  // create 100 of these elemenrs
+  //create 100 of these elements with a for loop
   for (let i = 0; i < 100; i++) {
+    //create element
     const square = document.createElement("div");
-     //create element
-
-     
     console.log(square);
-
+    //add styling to the element
+    square.classList.add("square");
+    //put the element into our grid
+    grid.appendChild(square);
+    //push it into a new squares array
+    squares.push(square);
   }
 }
 createGrid();
+
+currentSnake.forEach((index) => squares[index].classList.add("snake"));
+
+function move() {
+    
+}
